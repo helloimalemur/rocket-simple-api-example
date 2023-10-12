f7f93f13e95e1b36ba5b3b9195b230634f418ea7
helloimalemur <jkoontsiii@gmail.com>
2023-10-11 23:10:58 -0400

    .

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /srv/http/vhosts/rocket-sqlx-authentication-api/
-##./target/debug/rocket-sqlx-authentication-api
-#./rocket-sqlx-authentication-api
-/srv/http/vhosts/rocket-sqlx-authentication-api/exampleauth
-#
-#
